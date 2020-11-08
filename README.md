f1b2a8801b46c14c7fdc2f33c4358161f9477166
Hramos <ramoshumbertoa6@gmail.com>
2020-11-08 17:14:01 -0400

    add model

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
  
  model clasification with 83% accuracy
 
-![Model Accuracy](./img/Accuracy.JPG)
+ 
 
-![ROC](./img/ROC.JPG)
+![Model Accuracy](./img/Accuracy.JPG =250x250)
+
+![ROC](./img/ROC.JPG =250x250 )
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from app import app
+from flask import Flask
+from flask import render_template
 
-@app.route('/')
+# a route where we will display a welcome message via an HTML template
+@app.route("/")
+def hello():
+    message = "Hello, World"
+    return render_template('index.html', message=message)
 
-@app.route('/index')
-def index():
-    return "Hello world!!"
+# run the application
+if __name__ == "__main__":
+    app.run(debug=True)
